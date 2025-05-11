3655ad2deb3c986a5aac10020b4c1b52cee50f37
NataliaGumarova <n.gu.47943@gmail.com>
2025-05-11 16:31:05 +0400

    Задача 12

---
+++ src/Task12.java
@@ -0,0 +1,18 @@
+public class Task12 {
+    public static void main(String[] args) {
+        int[] mass = {1, 5, 3, 5, 1};
+        int t = mass.length / 2 - 1;//половина длинны массива -1, чтобы начинать проверку с mass[0]
+        boolean a = true;
+        two: for (int i = 0; i <= t; i++) {
+            if (mass[i] != mass[mass.length - 1 - i]) {
+                a = false;
+                break two;
+            }
+        }
+        if (a == true) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
